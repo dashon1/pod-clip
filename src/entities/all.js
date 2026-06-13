d4f716f36c82b3318ede72d78f264b1300db0ba2
dashon1 <dashon1@github.com>
2026-06-13 17:22:57 -0400

    fix: add integrations/Core.js stub

---
+++ src/entities/all.js
@@ -0,0 +1,5 @@
+import { base44 } from '../api/base44Client';
+export const Clip = base44.entities.Clip;
+export const Episode = base44.entities.Episode;
+export const Favorite = base44.entities.Favorite;
+export const User = base44.entities.User;
